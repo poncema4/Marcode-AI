8ae18bf07a9d2fc30e39c80b341b4f9ed186ed63
Marco Ponce <marcpon8@gmail.com>
2025-07-12 19:39:40 -0400

    trpc setup

--- marcode/src/app/page.tsx
+++ marcode/src/app/page.tsx
@@ -1,9 +1,18 @@
-const Page = () => {
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "Marco's Prefetch"}));
 
   return ( 
-    <div>
-      Hello world!
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+      <Client />
+      </Suspense>
+    </HydrationBoundary>
    );
 }
  
