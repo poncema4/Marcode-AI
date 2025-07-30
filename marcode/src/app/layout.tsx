d1307b2c757153456b54e6136c5b0e960d145616
Marco Ponce <marcpon8@gmail.com>
2025-07-29 20:29:20 -0400

    billing using clerk

--- marcode/src/app/layout.tsx
+++ marcode/src/app/layout.tsx
@@ -27,7 +27,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        variables: {
+          colorPrimary: "#12ADFF",
+        },
+      }}
+    >
       <TRPCReactProvider>
       <html lang="en" suppressHydrationWarning>
         <body
--- marcode/src/modules/home/ui/components/project-form.tsx
+++ marcode/src/modules/home/ui/components/project-form.tsx
@@ -39,8 +39,10 @@ export const ProjectForm = () => {
             queryClient.invalidateQueries(
                 trpc.projects.getMany.queryOptions(),
             );
+            queryClient.invalidateQueries(
+                trpc.usage.status.queryOptions(),
+            );
             router.push(`/projects/${data.id}`)
-            // TODO: Invalidate usage status
         },
         onError: (error) => {
             toast.error(error.message);
@@ -49,7 +51,9 @@ export const ProjectForm = () => {
                 clerk.openSignIn();
             }
 
-            // TODO; Redirect to pricing page if specific error
+            if (error.data?.code === "TOO_MANY_REQUESTS") {
+                router.push("/pricing")
+            }
         }
     }));
 
